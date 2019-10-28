82d2692bdfc951cb78cea80eb99bec4122ec1409
VinceMaestro <vincent.petit.danjou@gmail.com>
2019-10-28 03:28:54 +0100

    longestSubstringWithoutRepeatingCharacters & tests

---
+++ longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def initHashMap(self):
+        letter = 0
+        while (letter <= 255):
+            self.hashMap[chr(letter)] = -1
+            letter += 1
+
+    def lengthOfLongestSubstring(self, s):
+        self.hashMap = {}
+        self.initHashMap()
+        longest = 0
+        length = 0
+        i = 0
+        min = 0
+        max = len(s)
+        while (s and i < max):
+            if (self.hashMap[s[i]] < min):
+                length += 1
+            else:
+                length = i - self.hashMap[s[i]]
+                min = self.hashMap[s[i]]
+            longest = length if (length > longest) else longest
+            self.hashMap[s[i]] = i
+            i += 1
+        return longest
---
+++ longestSubstringWithoutRepeatingCharacters/test_longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,8 @@
+from longestSubstringWithoutRepeatingCharacters import Solution
+
+sol = Solution()
+print("Longest Substring in 'abba' is {}".format(sol.lengthOfLongestSubstring("abba")));
+print("Longest Substring in 'dvdf' is {}".format(sol.lengthOfLongestSubstring("dvdf")));
+print("Longest Substring in ' ' is {}".format(sol.lengthOfLongestSubstring(" ")));
+print("Longest Substring in 'bbbbbb' is {}".format(sol.lengthOfLongestSubstring("bbbbbb")));
+print("Longest Substring in 'pwwkew' is {}".format(sol.lengthOfLongestSubstring("pwwkew")));
