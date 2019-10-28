7868bd0b884300169ac5667f91a52c226d0327b8
VinceMaestro <vincent.petit.danjou@gmail.com>
2019-10-28 17:38:21 +0100

    v2 longestPalindromicSubstring, using Dynamic programming

--- longestPalindromicSubstring/longestPalindromicSubstring.py
+++ longestPalindromicSubstring/longestPalindromicSubstring.py
@@ -1,33 +1,50 @@
 class Solution:
-    def compare_str(self, first, last):
-        firstTmp = first
-        lastTmp = last
-        while (firstTmp < lastTmp and self.str[firstTmp] == self.str[lastTmp]):
-            firstTmp += 1
-            lastTmp -= 1
-        if (firstTmp >= lastTmp):
-            return last - first
-        else:
-            return 0
+    def __init__(self):
+        self.matrix = []
+        self.iMin = 0
+        self.jMax = 0
+
+    def initMatrix(self, lenght):
+        for i in range(0, lenght):
+            self.matrix.append([])
+            for j in range(0, lenght):
+                if (i == j):
+                    self.matrix[i].append(True)
+                elif ((j == i + 1) and (self.str[i] == self.str[j])):
+                    self.matrix[i].append(True)
+                    if (j - i > self.jMax - self.iMin):
+                        self.jMax = j
+                        self.iMin = i
+                else:
+                    self.matrix[i].append(False)
+
+    def findAllPalindromes(self, lenght):
+        offset = 2
+        i = 0
+        j = offset
+        while (i < lenght - offset or j < lenght):
+            if (j >= i):
+                if (self.matrix[i + 1][j - 1] == True and self.str[i] == self.str[j]):
+                    self.matrix[i][j] = True
+                    if (j - i > self.jMax - self.iMin):
+                        self.jMax = j
+                        self.iMin = i
+                else:
+                    self.matrix[i][j] = False
+            i += 1
+            j += 1
+            if (j == lenght and i == lenght - offset):
+                offset += 1
+                j = offset
+                i = 0
 
     def longestPalindrome(self, s):
-        if (s):
+        if (s and s != s[::-1]):
             self.str = s
-            iFirstPtrFinal = 0
-            iLastPtrFinal = 0
-            iFirstPtrTmp = 0
-            while (iFirstPtrTmp < len(self.str) and (iLastPtrFinal - iFirstPtrFinal != len(self.str) - 1)):
-                iLastPtrTmp = len(self.str) - 1
-                while (iLastPtrTmp > iFirstPtrTmp):
-                    if (self.compare_str(iFirstPtrTmp, iLastPtrTmp) > iLastPtrFinal - iFirstPtrFinal):
-                        iLastPtrFinal = iLastPtrTmp
-                        iFirstPtrFinal = iFirstPtrTmp
-                    iLastPtrTmp -= 1
-                iFirstPtrTmp += 1
-            ret = ""
-            while (iFirstPtrFinal <= iLastPtrFinal):
-                ret += self.str[iFirstPtrFinal]
-                iFirstPtrFinal += 1
-            return ret
+            lenght = len(self.str)
+            self.initMatrix(lenght)
+            self.findAllPalindromes(lenght)
+            longest = s[self.iMin: self.jMax + 1]
+            return(longest)
         else:
-            return s
+            return(s)
